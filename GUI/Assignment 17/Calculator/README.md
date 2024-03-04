bce55efdf972007962c984ae103edecb781e2aa7
Moein-Moatali-git <MoeinMoatali@gmail.com>
2024-03-04 20:33:04 +0330

    GUI

---
+++ GUI/Assignment 17/Calculator/README.md
@@ -0,0 +1,18 @@
+# Calculator
+Calculator or features (addition, subtraction, multiplication, division, sine, cosine, tangent, cotangent, logarithm, sqrt, etc.)
+## I used the following libraries:
+```
+QT
+PySide
+math
+sympy
+```
+## How to run calculator?
+You must first install all libraries, then enter the following command in the terminal:
+```
+python main.py
+```
+
+## 
+
+![" Calculator "]()
--- GUI/Assignment 17/Calculator/widget.py
+++ GUI/Assignment 17/Calculator/widget.py
@@ -1,3 +1,5 @@
+import math
+from sympy import cot
 from PySide6.QtWidgets import QWidget,QPushButton
 from ui_widget import Ui_widget
 
@@ -8,9 +10,9 @@ class Widget(QWidget,Ui_widget):
         self.setWindowTitle("Moein Calculator")
         self.setGeometry(200,455,300,450)
         self.setMaximumHeight(450)
-        self.setMaximumWidth(300)
+        self.setMaximumWidth(400)
         self.setMinimumHeight(450)
-        self.setMinimumWidth(300)
+        self.setMinimumWidth(400)
 
         self.button_0.clicked.connect(self.on_button_click)
         self.button_1.clicked.connect(self.on_button_click)
@@ -27,8 +29,15 @@ class Widget(QWidget,Ui_widget):
         self.button_plus.clicked.connect(self.on_button_click)
         self.button_minus.clicked.connect(self.on_button_click)
         self.button_division.clicked.connect(self.on_button_click)
-        self.button_multiply.connect(self.on_button_click)
         self.button_equals.clicked.connect(self.on_button_click)
+        self.button_sin.clicked.connect(self.on_button_click)
+        self.button_cos.clicked.connect(self.on_button_click)
+        self.button_tan.clicked.connect(self.on_button_click)
+        self.button_cot.clicked.connect(self.on_button_click)
+        self.button_log.clicked.connect(self.on_button_click)
+        self.button_sqrt.clicked.connect(self.on_button_click)
+        
+
 
     def on_button_click(self):
         button = self.sender()
@@ -43,9 +52,34 @@ class Widget(QWidget,Ui_widget):
                 self.main_label.setText(result)
                 self.main_label_2.setText(current_text + button.text())
             except ZeroDivisionError:
-                self.main_label.setText("Error: Division by zero")
+                self.main_label.setText("Error: 0/number")
             except Exception as e:
-                self.main_label.setText("Error: Invalid expression")
+                self.main_label.setText("Error")
+        elif button.text() == "sin":
+            result=str(eval(current_text))
+            number=math.sin(eval(result))
+            self.main_label.setText(str(number))
+        elif button.text() == "cos":
+            result=str(eval(current_text))
+            number=math.cos(eval(result))
+            self.main_label.setText(str(number))
+        elif button.text() == "tan":
+            result=str(eval(current_text))
+            number=math.tan(eval(result))
+            self.main_label.setText(str(number))
+        elif button.text() == "cot":
+            result=str(eval(current_text))
+            # there is not cot in math library , i use sympy
+            number=cot(eval(result))
+            self.main_label.setText(str(number))
+        elif button.text() == "log":
+            result=str(eval(current_text))
+            number=math.log(eval(result))
+            self.main_label.setText(str(number))
+        elif button.text() == "sqrt":
+            result=str(eval(current_text))
+            number=math.sqrt(eval(result))
+            self.main_label.setText(str(number))
         else:
             self.main_label.setText(current_text + button.text())
             self.main_label_2.setText(current_text + button.text())
