08f5a44963f84db6df15cdb9b3106431dcd32614
Moein-Moatali-git <MoeinMoatali@gmail.com>
2024-04-09 23:25:15 +0330

    A-18

--- GUI/Assigenment 18/Tic Tac Toe/main.py
+++ GUI/Assigenment 18/Tic Tac Toe/main.py
@@ -1,151 +1,129 @@
-import random
 import sys
 from functools import partial
-from PySide6.QtWidgets import QApplication,QMessageBox
+import random
+from PySide6.QtWidgets import QApplication , QMessageBox
 from PySide6.QtUiTools import QUiLoader
 
-player_1_score = 0
-player_2_score = 0
+def Check():
 
-def cpu():
-    for i in range(3):
-            for x in range(3):
-                buttons[i][x].clicked.connect(partial(paly,random.randint(0,2),random.randint(0,2)))
-
-
-def check_x():
-        global player_1_score
-        msg_box=QMessageBox(text="player X wine! 🤩 😍 ❤️ 🌹 ")
-        msg_box.show()
-        player_1_score += 1
-        msg_box.exec()
-
-def check_O():
-        global player_2_score
-        msg_box=QMessageBox(text="player O wine! 🤩 😍 ❤️ 🌹 ")
-        msg_box.show()
-        player_2_score += 1
-        msg_box.exec()
-
-def check():
-    global player_1_score
-    global player_2_score
-    if buttons[0][0].text()=="X" and buttons[0][1].text()=="X" and buttons[0][2].text()=="X":
-        check_x()
-        return
-    elif buttons[1][0].text()=="X" and buttons[1][1].text()=="X" and buttons[1][2].text()=="X":
-        check_x()
-        return
-    elif buttons[2][0].text()=="X" and buttons[2][1].text()=="X" and buttons[2][2].text()=="X":
-        check_x()
-        return
-    elif buttons[0][0].text()=="X" and buttons[1][0].text()=="X" and buttons[2][0].text()=="X":
-        check_x()
-        return
-    elif buttons[0][1].text()=="X" and buttons[1][1].text()=="X" and buttons[2][1].text()=="X":
-        check_x()
-        return
-    elif buttons[0][2].text()=="X" and buttons[1][2].text()=="X" and buttons[2][2].text()=="X":
-        check_x()
-        return
-    elif buttons[0][0].text()=="X" and buttons[1][1].text()=="X" and buttons[2][2].text()=="X":
-        check_x()
-        return
-    elif buttons[0][2].text()=="X" and buttons[1][1].text()=="X" and buttons[2][0].text()=="X":
-        check_x()
-        return
-    #
-    if buttons[0][0].text()=="O" and buttons[0][1].text()=="O" and buttons[0][2].text()=="O":
-        check_O()
-        return
-    elif buttons[1][0].text()=="O" and buttons[1][1].text()=="O" and buttons[1][2].text()=="O":
-        check_O()
-        return
-    elif buttons[2][0].text()=="O" and buttons[2][1].text()=="O" and buttons[2][2].text()=="O":
-        check_O()
-        return
-    elif buttons[0][0].text()=="O" and buttons[1][0].text()=="O" and buttons[2][0].text()=="O":
-        check_O()
-        return
-    elif buttons[0][1].text()=="O" and buttons[1][1].text()=="O" and buttons[2][1].text()=="O":
-        check_O()
-        return
-    elif buttons[0][2].text()=="O" and buttons[1][2].text()=="O" and buttons[2][2].text()=="O":
-        check_O()
-        return
-    elif buttons[0][0].text()=="O" and buttons[1][1].text()=="O" and buttons[2][2].text()=="O":
-        check_O()
-        return
-    elif buttons[0][2].text()=="O" and buttons[1][1].text()=="O" and buttons[2][0].text()=="O":
-        check_O()
-        return
-
-    count=0
-    for i in range(3):
-        for j in range(3):
-            if buttons[i][j].text() != "":
-                count+=1
-
-    if count == 9:
-        msg_box=QMessageBox(text=" You are Equal 🌹 ")
-        msg_box.show()
-        msg_box.exec()
-
-def about():
-    msg_box =QMessageBox(text="This is the TIC TAC TOE game ❤️")
-    msg_box.show()
-    msg_box.exec()
-
-def paly(row,col):
-    global player
-    global buttons
-    if player == 1:
-        buttons[row][col].setText("X")
-        buttons[row][col].setStyleSheet("background-color:pink;color:red;")
-        player=2
-    elif player == 2:
-        buttons[row][col].setText("O")
-        buttons[row][col].setStyleSheet("background-color:lightblue;color:blue;")
-        player=1
-
-    check()
-    #glo ... 
-    main_window.lineEdit_X.setText(str(player_1_score))
-    main_window.lineEdit_O.setText(str(player_2_score))
-  
-def new_game():
-    for i in range(3):
-        for x in range(3):
-            buttons[i][x].setText("")
-            buttons[i][x].setStyleSheet("background-color:white;")
-
-def on_toggled(checked):
-    if checked:
+    global Player
+    checklist=["X","O"]
+    
+    global msg_box, win_game
+    for j in range(len(checklist)):
         for i in range(3):
-            for x in range(3):
-                buttons[i][x].clicked.connect(partial(paly,i,x))
-    else:
+            if  buttons[i][0].text()==checklist[j]  and buttons[i][1].text()==checklist[j] and buttons[i][2].text()==checklist[j] or\
+                buttons[0][i].text()==checklist[j]  and buttons[1][i].text()==checklist[j] and buttons[2][i].text()==checklist[j] or\
+                buttons[0][0].text()==checklist[j]  and buttons[1][1].text()==checklist[j] and buttons[2][2].text()==checklist[j] or\
+                buttons[0][2].text()==checklist[j]  and buttons[1][1].text()==checklist[j] and buttons[2][0].text()==checklist[j] :
+                msg_box=QMessageBox(text=f"player{j+1} wine! 🌹 🤩 !")
+                win_game=1
+                msg_box.show()
+                if j+1==1:
+                    text=main_window.xscore.text()
+                    text1=text.split(":")
+                    main_window.xscore.setText(f"X Score: {int(text1[1])+1}")
+                    Player=1
+                    break
+                elif j+1==2:
+                    text=main_window.oscore.text()
+                    text1=text.split(":")
+                    main_window.oscore.setText(f"O Score: {int(text1[1])+1}")
+                    Player=2
+                    break
+    if len(button_clicked)==9 and win_game!=1:
+            msg_box=QMessageBox(text= "Ties!")
+            text=main_window.oscore.text()
+            text1=text.split(":")
+            main_window.tie.setText(f"Ties: {int(text1[1])+1}")
+            win_game=2
+            msg_box.show()
+    if Player==2:
         cpu()
 
+def play(row,col):
+    global Player, win_game
+    global buttons
+    if win_game==0:
+        for i in button_clicked:
+            if i==buttons[row][col]:
+                break
+        else:
+            if Player==1:
+                buttons[row][col].setText("X")
+                buttons[row][col].setStyleSheet("color : rgb(255, 0, 0);")
+                main_window.turn.setText(f"O`s Turn")
+                Player=2  
+            elif Player==2:
+                buttons[row][col].setText("O")
+                buttons[row][col].setStyleSheet("color : rgb(0, 0, 255);")
+                main_window.turn.setText(f"X`s Turn")
+                Player=1 
+            button_clicked.append(buttons[row][col])
 
-loader=QUiLoader()
-app=QApplication(sys.argv)
-main_window=loader.load("main.ui")
+        Check() 
 
+def about():
+    about_box = QMessageBox(text = "This is Tic Tac Toe game ❤️")
+    about_box.setWindowTitle("Tic Tac Toe")
+    about_box.exec()
+
+def cpu():
+    global win_game, Player
+    if main_window.cpu.isChecked() and win_game==0:
+        if len(button_clicked)==0:
+            btn_cpu=buttons[random.randint(0,2)][random.randint(0,2)]
+            btn_cpu.setText("O")
+            btn_cpu.setStyleSheet("color : rgb(0, 0, 255);")
+        else:
+            while True:
+                btn_cpu=buttons[random.randint(0,2)][random.randint(0,2)]
+                for k in button_clicked:
+                    if btn_cpu==k:
+                        break
+                else:
+                    btn_cpu.setText("O")
+                    btn_cpu.setStyleSheet("color : rgb(0, 0, 255);")
+                    break
+        main_window.turn.setText(f"X`s Turn")
+        Player=1 
+        button_clicked.append(btn_cpu)
+        Check()
+
+def new_game():
+    global win_game, Player
+    for i in range(3):
+        for j in range(3):
+            buttons[i][j].setText("")
+            buttons[i][j].setStyleSheet("background-color: rgb(51, 214, 206); border-radius: 10px; border-style: outset; border-width:3px; border-color: rgb(255, 255, 255);")
+    button_clicked.clear()
+    win_game=0
+    if Player==1:
+        main_window.turn.setText(f"X`s Turn")
+    elif Player==2:
+        main_window.turn.setText(f"O`s Turn")
+    if Player==2 and main_window.cpu.isChecked():
+        Check()
+
+loader = QUiLoader()
+app = QApplication(sys.argv)
+Player = 1 
+win_game=0
+
+main_window = loader.load("main.ui")
 main_window.show()
-player=random.randint(1,2)
-
-buttons=[[main_window.btn_1,main_window.btn_2,main_window.btn_3],
-         [main_window.btn_4,main_window.btn_5,main_window.btn_6],
-         [main_window.btn_7,main_window.btn_8,main_window.btn_9]]
+main_window.setWindowTitle("Tic Tac Toe")
 
 
-radio_button_friend=main_window.radio_friend
-radio_button_friend.toggled.connect(partial(on_toggled))
+button_clicked=[]
+buttons = [[main_window.btn_1 , main_window.btn_2 , main_window.btn_3], 
+           [main_window.btn_4 , main_window.btn_5 , main_window.btn_6],
+           [main_window.btn_7 , main_window.btn_8 , main_window.btn_9]]
 
-
-
-main_window.btn_about.clicked.connect(partial(about))
-main_window.btn_new_game.clicked.connect(partial(new_game))
-
+for i in range(3):
+    for j in range(3):
+        buttons[i][j].clicked.connect(partial(play , i , j))
+   
+main_window.about.clicked.connect(about)
+main_window.newgame.clicked.connect(new_game)       
 app.exec()
