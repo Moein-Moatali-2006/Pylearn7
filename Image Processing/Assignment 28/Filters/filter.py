270f03fab7ad3e5e05e016d9d90f60b1d36fb487
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2024-09-05 20:33:04 +0330

    A-28

--- Image Processing/Assignment 28/Filters/filter.py
+++ Image Processing/Assignment 28/Filters/filter.py
@@ -1,26 +1,42 @@
-import cv2 as cv
+import cv2
 
 
-cap=cv.VideoCapture(0)
+face_sticker = cv2.imread("input\image_lion.png")
 
-face_detector=cv.CascadeClassifier("Filters\models\haarcascade_frontalface_alt.xml")
-img_lion=cv.imread("Filters\input\lion.jpg")
+
+face_detect = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+cap = cv2.VideoCapture(0)
+
+face_sticker_active = False
 
 while True:
-    _,frame=cap.read()
-    key=cv.waitKey(1)
-    if key == ord('q'):
+    _, frame = cap.read()
+    if not _:
         break
-    elif key == ord('1'):
-        ...         
-    elif key == ord('2'):
-        ...
-    elif key == ord('3'):
-        ...
-    elif key == ord('4'):
-        ...
-    
-    cv.imshow("result",frame)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detect.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+    if face_sticker_active:
+        for (x, y, w, h) in faces:
+            resized_sticker = cv2.resize(face_sticker, (w, h))
+
+            for i in range(resized_sticker.shape[0]):
+                for j in range(resized_sticker.shape[1]):
+                    if all(resized_sticker[i, j] != 0):
+                        frame[y + i, x + j] = resized_sticker[i, j]
+
+    cv2.imshow("Webcam", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('1'):
+        face_sticker_active = True
+    elif key == ord('q'):
+        break
+
 
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
