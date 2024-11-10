6d2e5769d167f7465dfc658a12e778bcef1a74ac
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2024-11-10 22:29:12 +0330

    A-33

---
+++ Image Processing/Object Detection/boundingrect/main.py
@@ -0,0 +1,56 @@
+
+import cv2
+import numpy as np
+
+def my_bounding_rect(contour):
+    x_min = contour[0][0]
+    y_min = contour[0][1]
+    x_max = contour[0][0]
+    y_max = contour[0][1]
+    
+    for list in contour:
+        
+        if x_min < list[0]:
+            x_min = x_min
+        else:
+            x_min = list[0]
+
+        if y_min < list[1]:
+            y_min = y_min
+        else:
+            y_min = list[1]
+
+        if x_max > list[0]:
+            x_max = x_max
+        else:
+            x_max = list[0]
+
+        if y_max > list[1]:
+            y_max = y_max
+        else :
+            y_max = list[1]
+    
+    w = x_max - x_min
+    h = y_max - y_min
+
+    return x_min,y_min,w+1,h+1
+
+contour=[(100, 150), (150, 200), (200, 150), (150, 100), (250, 200), (220, 200)]
+x,y,w,h = my_bounding_rect(contour)
+
+print(f"x:{x}")
+print(f"y:{y}")
+print(f"w:{w}")
+print(f"h:{h}")
+
+print("------------------------------------")
+
+
+contour = np.array([[(100, 150), (150, 200), (200, 150), (150, 100), (250, 200), (220, 200)]], dtype=np.int32)
+
+x, y, w, h = cv2.boundingRect(contour)
+print(f"x: {x}")
+print(f"y: {y}")
+print(f"w: {w}")
+print(f"h: {h}") 
+
